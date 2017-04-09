0431eee80cff7ca2111f795051c0df9a1ad2d84f
Note-do-Sett7 <Note-do-Sett7@DESKTOP-0E5FL1I>
2017-04-09 20:11:17 -0300

    Projeto de Visão da aplicação boolixo mais atualizada

--- BolichoVisao/src/br/edu/iffarroupilha/bolicho/visao/desktop/FrmCliente.java
+++ BolichoVisao/src/br/edu/iffarroupilha/bolicho/visao/desktop/FrmCliente.java
@@ -6,14 +6,20 @@
 
 package br.edu.iffarroupilha.bolicho.visao.desktop;
 
+import br.edu.iffarroupilha.bolicho.controle.ClientesControle;
+import br.edu.iffarroupilha.bolicho.modelo.Cliente;
+import com.sun.security.ntlm.Client;
 import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 /**
@@ -49,6 +55,27 @@ public class FrmCliente extends JFrame{
         
         setLayout(new FlowLayout(FlowLayout.LEFT));
         
+        bntGravar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ClientesControle control = new ClientesControle();
+                // pegar os dados em tela
+                Cliente C = new Cliente();
+                C.setNome(txtNome.getText());
+                int cpf = control.stringToInt(txtCpf.getText());//converto o texto de cpf para inteiro
+                C.setCpf(cpf);
+                //valida os dados
+                String erro = control.validarDados(C);
+                if(erro== null){
+                    // sem erro
+                    control.gravar(C);
+                JOptionPane.showMessageDialog(null, "Gravou Parsa");
+            
+                }else{
+                    JOptionPane.showMessageDialog(null, erro, "Validação", JOptionPane.ERROR_MESSAGE);
+                }
+             }
+        });
         // adiciona os componentes em tela
         
         add(lblCpf);
--- BolichoVisao/src/br/edu/iffarroupilha/bolicho/visao/desktop/FrmProduto.java
+++ BolichoVisao/src/br/edu/iffarroupilha/bolicho/visao/desktop/FrmProduto.java
@@ -6,11 +6,16 @@
 
 package br.edu.iffarroupilha.bolicho.visao.desktop;
 
+import br.edu.iffarroupilha.bolicho.controle.ProdutoControle;
+import br.edu.iffarroupilha.bolicho.modelo.Produto;
 import java.awt.FlowLayout;
 import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 /**
@@ -42,6 +47,26 @@ public class FrmProduto extends JFrame{
         JTextField jtlPreco = new JTextField(30);
         JButton btnGravar = new JButton("Grevar");
         
+        btnGravar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ProdutoControle produto = new ProdutoControle();
+                Produto p = new Produto();
+                p.setDescricao(jtlDescricao.getText());
+                double preco = produto.stringToInt(jtlPreco.getText());
+                p.setPreco(preco);
+                 String erro = produto.validarDados(p);
+                if(erro== null){
+                    // sem erro
+                    produto.gravar(p);
+                JOptionPane.showMessageDialog(null, "Gravou Parsa");
+            
+                }else{
+                    JOptionPane.showMessageDialog(null, erro, "Validação", JOptionPane.ERROR_MESSAGE);
+                }
+                
+            }
+        });
         setLayout(new FlowLayout(FlowLayout.LEFT));
         
         add(jblDescricao);
